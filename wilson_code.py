cfbd25b275c741db7fd53dbe3e63d3cea0bf8143
Jack Poleto <jjap2021@gmail.com>
2024-02-20 13:18:38 -0500

    i want to break it

--- wilson_code.py
+++ wilson_code.py
@@ -1,4 +1,5 @@
 import antigravity
+import random
 
 # Yes, that actually works in Python
 
@@ -6,3 +7,9 @@ print('bye')
 
 if __name__ == '__main__':
     print('this is a main block')
+
+
+#Jack's
+random_number = random.randint(1, 100)
+
+print("Random number:", random_number)
